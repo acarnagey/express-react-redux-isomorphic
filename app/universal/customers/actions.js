72af56273f794ff3564ee79180e4929222b8bf89
Adam Carnagey <carnagey.adam@gmail.com>
2020-12-10 12:53:41 -0600

    redux-thunk with async/await for actions

--- app/universal/customers/actions.js
+++ app/universal/customers/actions.js
@@ -5,60 +5,44 @@ import api from "../api";
 
 export const { resultsUpdated, setPage } = actions("customers");
 
-export function create(data) {
-  return () =>
-    api.customers
-      .create(data)
-      .then(() => "SUCCESS")
-      .catch((err) => {
-        if (err.response.status === 422) {
-          throw new SubmissionError(err.response.data.errors);
-        }
-        throw err;
-      });
+export const create = (data) => {
+  return async () => {
+    try {
+      await api.customers.create(data);
+      return "SUCCESS";
+    } catch (err) {
+      if (err.response.status === 422) {
+        throw new SubmissionError(err.response.data.errors);
+      }
+      throw err;
+    }
+  }
 }
 
 export const update = (data) => {
-  return () =>
-    api.customers
-      .update(data)
-      .then(() => {
-        return "SUCCESS";
-      })
-      .catch((err) => {
-        if (err.response.status === 422) {
-          throw new SubmissionError(err.response.data.errors);
-        }
-        throw err;
-      });
+  return async () => {
+    try {
+      await api.customers.update(data);
+      return "SUCCESS";
+    } catch (err) {
+      if (err.response.status === 422) {
+        throw new SubmissionError(err.response.data.errors);
+      }
+      throw err;
+    }
+  };
 };
 
 export const deleteById = (id) => {
-  return () =>
-    api.customers.deleteById(id).then(() => {
-      return "SUCCESS";
-    });
+  return async () => {
+    await api.customers.deleteById(id);
+    return "SUCCESS";
+  };
 };
 
-// export function resultsUpdated({ results, totalCount }) {
-//   return {
-//     type: actionTypes.RESULTS_UPDATED,
-//     results,
-//     totalCount,
-//   };
-// }
-
-export function list(page = 1, pageSize = 15) {
-  return (dispatch) =>
-    api.customers.list(page, pageSize).then((resp) => {
-      dispatch(resultsUpdated(resp.data));
-    });
-}
-
-// export function previous() {
-//   return { type: actionTypes.PREV };
-// }
-
-// export function next() {
-//   return { type: actionTypes.NEXT };
-// }
+export const list = (page = 1, pageSize = 15) => {
+  return async (dispatch) => {
+    const resp = await api.customers.list(page, pageSize);
+    dispatch(resultsUpdated(resp.data));
+  };
+};
--- app/universal/invoices/actions.js
+++ app/universal/invoices/actions.js
@@ -5,21 +5,22 @@ import api from "../api";
 export const { resultsUpdated } = actions("invoices");
 
 export function create(data) {
-  return () =>
-    api.invoices
-      .create(data)
-      .then(() => "SUCCESS")
-      .catch((err) => {
-        if (err.response.status === 422) {
-          throw new SubmissionError(err.response.data.errors);
-        }
-        throw err;
-      });
+  return async () => {
+    try {
+      await api.invoices.create(data);
+      return "SUCCESS";
+    } catch (err) {
+      if (err.response.status === 422) {
+        throw new SubmissionError(err.response.data.errors);
+      }
+      throw err;
+    }
+  }
 }
 
-export function list(page = 1, pageSize = 15) {
-  return (dispatch) =>
-    api.invoices.list(page, pageSize).then((resp) => {
-      dispatch(resultsUpdated(resp.data));
-    });
+export const list = (page = 1, pageSize = 15) => {
+  return async (dispatch) => {
+    const resp = await api.invoices.list(page, pageSize);
+    dispatch(resultsUpdated(resp.data));
+  }
 }
